5782824e49ea22a8e67375896c8aeb077f34af8b
Peter Dravitzki <delcnz06@gmail.com>
2024-07-11 19:56:19 +1200

    update:deployed to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/todo/',
+  build: {
+    outDir: 'dist',
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
